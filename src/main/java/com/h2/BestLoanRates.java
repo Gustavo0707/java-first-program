a3830daad64977125267c7d1ad9c38d5ea51c419
Gustavo0707 <gustavomelo0703@gmail.com>
2021-04-23 15:53:08 -0300

    Alterações

---
+++ src/main/java/com/h2/BestLoanRates.java
@@ -0,0 +1,35 @@
+package com.h2;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class BestLoanRates {
+    public static final Map<Integer, Float> bestRates = Map.of(
+            1, 5.50f,
+            2, 3.45f,
+            3, 2.67f
+    );
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Enter your name");
+        String name = scanner.nextLine()
+        System.out.println("Hello " + name);
+
+        System.out.println("Enter the loan term (in years");
+        int loanTermsInYears = scanner.nextInt();
+        float bestRate = getRates(loanTermsInYears);
+        if(bestRate == 0.0f) {
+            System.out.println("no available rates for term "+ loanTermsInYears + " years");
+        } else {
+            System.out.println("Best Available Rate"+ getRates(loanTermsInYears) + "%");
+        }
+    }
+
+    public static float getRates(int loanTermsInYears) {
+        if(bestRates.containsKey(loanTermsInYears)) {
+            return bestRates.get(loanTermsInYears);
+        }
+        return 0.0f;
+    }
+}
