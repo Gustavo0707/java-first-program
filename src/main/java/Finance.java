8c3482488ec716a94a5df238675a3603674be03d
Gustavo0707 <gustavomelo0703@gmail.com>
2021-04-26 16:02:01 -0300

    Update Finance.java

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -13,7 +13,7 @@ public class Finance {
     public static final Map<String, String> commandsToUsage = Map.of(
             BEST_LOAN_RATES, "usage: bestLoanRate",
             SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
-            MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>" 
+            MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>"
     );
 
     private static boolean validateCommandArguments(String[] args) {
